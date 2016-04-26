a6889388bbd6f3bb1b29b6bfac2da4e9cd826647
ibarbech <ibarbech@alumnos.unex.es>
2016-04-26 18:07:16 +0200

    hola

--- LegControllerComp/borrar_para_subir.sh
+++ LegControllerComp/borrar_para_subir.sh
@@ -1,3 +1,3 @@
-#make clean
-git rm -rf src/*~ *~ etc/*~ src/Makefile src/cmake_install.cmake Makefile CMakeFiles bin CMakeCache.txt src/moc_* cmake_install.cmake *.kdev4 src/CMakeFiles src/*.new* cmake_install.cmake *.new*
+make clean
+rm -rf src/*~ *~ etc/*~ src/Makefile src/cmake_install.cmake Makefile CMakeFiles bin CMakeCache.txt src/moc_* cmake_install.cmake *.kdev4 src/CMakeFiles src/*.new* cmake_install.cmake *.new*
 
